fbab964e4105d3389664a34ece78359cd6db2698
João Bordalo <bordalix@users.noreply.github.com>
2024-10-04 20:17:43 +0100

    change to signet

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,6 +3,6 @@ import { NetworkName } from './network'
 
 export const defaultDust = 450
 export const defaultExplorer = ExplorerName.Mempool
-export const defaultNetwork = NetworkName.Regtest
+export const defaultNetwork = NetworkName.Signet
 export const defaultMinRelayFee = 30
 export const defaultRoundInterval = 10
