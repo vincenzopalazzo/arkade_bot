5d263be53125ba68f1ae67903996b2508d87c4d1
João Bordalo <bordalix@users.noreply.github.com>
2024-10-07 11:18:01 +0200

    don't show explorer button when receiving

--- src/screens/Wallet/Receive/Success.tsx
+++ src/screens/Wallet/Receive/Success.tsx
@@ -6,19 +6,9 @@ import ButtonsOnBottom from '../../../components/ButtonsOnBottom'
 import { NavigationContext, Pages } from '../../../providers/navigation'
 import Content from '../../../components/Content'
 import Container from '../../../components/Container'
-import { openInNewTab } from '../../../lib/explorers'
-import { FlowContext } from '../../../providers/flow'
-import { WalletContext } from '../../../providers/wallet'
 
 export default function ReceiveSuccess() {
-  const { recvInfo } = useContext(FlowContext)
   const { navigate } = useContext(NavigationContext)
-  const { wallet } = useContext(WalletContext)
-
-  const handleExplorer = () => {
-    if (!recvInfo.txid) return
-    openInNewTab(recvInfo.txid, wallet)
-  }
 
   const goBackToWallet = () => navigate(Pages.Wallet)
 
