7de5155e248bf245c4aaae988ec9234b08251a6f
João Bordalo <bordalix@users.noreply.github.com>
2024-10-07 11:17:32 +0200

    bug fix with error extraction

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -1,4 +1,5 @@
 export const extractError = (error: any): string => {
+  if (error.message) return error.message
   if (error?.response?.data?.error) return error.response.data.error
   if (typeof error === 'string') return error
   return JSON.stringify(error)
