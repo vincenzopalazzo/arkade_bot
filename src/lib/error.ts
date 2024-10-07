2770070b7eac5c0b1bcb366b1d07b9cbc22744c6
João Bordalo <bordalix@users.noreply.github.com>
2024-10-07 19:31:35 +0200

    better error

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -1,6 +1,6 @@
 export const extractError = (error: any): string => {
-  if (error.message) return error.message
   if (error?.response?.data?.error) return error.response.data.error
   if (typeof error === 'string') return error
+  if (error.message) return error.message
   return JSON.stringify(error)
 }
