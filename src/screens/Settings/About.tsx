133becee7eb8fb3dbd9eec040182046515352d8e
João Bordalo <bordalix@users.noreply.github.com>
2024-10-14 15:59:24 +0100

    new about page

--- src/screens/Settings/About.tsx
+++ src/screens/Settings/About.tsx
@@ -5,18 +5,32 @@ import Title from '../../components/Title'
 import { ConfigContext } from '../../providers/config'
 import Content from '../../components/Content'
 import Container from '../../components/Container'
+import { AspContext } from '../../providers/asp'
+import Table from '../../components/Table'
+import { prettyLongText } from '../../lib/format'
 
 export default function About() {
+  const { aspInfo } = useContext(AspContext)
   const { toggleShowConfig } = useContext(ConfigContext)
+
+  const days = Math.round(aspInfo.roundLifetime / 60 / 60 / 24)
+
+  const data = [
+    ['Dust', `${aspInfo.dust} sats`],
+    ['Min Relay Fee', `${aspInfo.minRelayFee} sats`],
+    ['Pubkey', prettyLongText(aspInfo.pubkey)],
+    ['Round Interval', `${aspInfo.roundInterval} secs`],
+    ['Round Lifetime', `${days} days`],
+    ['Unilateral Exit Delay', `${aspInfo.unilateralExitDelay} secs`],
+    ['URL', aspInfo.url],
+  ]
 
   return (
     <Container>
       <Content>
-        <Title text='About' />
-        <div className='flex flex-col gap-2'>
-          <p>Ark wallet PoC</p>
-          <p>Extremely beta software</p>
-          <p>Signet only</p>
+        <Title text='About' subtext='Ark wallet PoC' />
+        <div className='mt-10'>
+          <Table data={data} />
         </div>
       </Content>
       <ButtonsOnBottom>
