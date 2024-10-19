15e69695f5396bf46b07a6f91641071641dc4c7a
João Bordalo <bordalix@users.noreply.github.com>
2024-10-19 13:05:18 +0100

    allow for disabled <selects>

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -3,19 +3,20 @@ import Label from './Label'
 
 interface SelectProps {
   children: ReactNode
+  disabled?: boolean
   label?: string
   onChange: (arg0: any) => void
   value: string | number
 }
 
-export default function Select({ children, label, onChange, value }: SelectProps) {
+export default function Select({ children, disabled, label, onChange, value }: SelectProps) {
   const className =
     'bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-600 text-gray-800 dark:text-gray-100 text-lg rounded-lg w-full p-2.5'
 
   return (
     <div>
       {label ? <Label text={label} /> : null}
-      <select className={className} onChange={onChange} value={value}>
+      <select className={className} onChange={onChange} value={value} disabled={disabled}>
         {children}
       </select>
     </div>
