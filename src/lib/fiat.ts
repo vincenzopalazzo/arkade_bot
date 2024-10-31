a1335ba232ba325f7927055ab7814414fda28175
João Bordalo <bordalix@users.noreply.github.com>
2024-10-31 15:50:29 +0000

    fix btc price oracle

--- src/lib/fiat.ts
+++ src/lib/fiat.ts
@@ -14,10 +14,12 @@ export interface OracleResponse {
 
 export const getPriceFeed = async (): Promise<FiatPrices | undefined> => {
   try {
-    const { pricefeed }: OracleResponse = await fetch('https://btcoracle.bordalix.workers.dev/').then((res) =>
-      res.json(),
-    )
-    return pricefeed
+    const resp = await fetch('https://blockchain.info/ticker')
+    const json = await resp.json()
+    return {
+      eur: json.EUR?.last,
+      usd: json.USD?.last,
+    }
   } catch (err) {
     console.log('error fetching fiat prices:', extractError(err))
   }
