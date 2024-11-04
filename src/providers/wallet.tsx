0a58c0596ede5ec5f813498bbda053d8a22d209a
João Bordalo <bordalix@users.noreply.github.com>
2024-11-04 09:34:20 +0000

    use r2 for wasm file

--- src/providers/wallet.tsx
+++ src/providers/wallet.tsx
@@ -99,8 +99,10 @@ export const WalletProvider = ({ children }: { children: ReactNode }) => {
   useEffect(() => {
     if (wasmLoaded) return
     const go = new window.Go()
-    const devMode = true
-    const url = devMode ? 'ark-sdk.wasm' : 'https://arkadewasm.bordalix.workers.dev?21'
+    const devMode = false
+    const sdkFile = 'ark-sdk-04e57f81.wasm'
+    const r2 = 'https://pub-2691569bbfd24a6a81b70001c8eb7506.r2.dev/'
+    const url = devMode ? sdkFile : r2 + sdkFile
     WebAssembly.instantiateStreaming(fetch(url), go.importObject).then((result) => {
       go.run(result.instance)
       setWasmLoaded(true)
