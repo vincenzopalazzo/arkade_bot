e0d2c55526a855679d933a9cf456ca652e999669
João Bordalo <bordalix@users.noreply.github.com>
2024-11-06 13:56:27 +0000

    add comment

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -7,6 +7,9 @@ export const fetchURL = async (url: string): Promise<any> => {
   return (await res.json()) as any
 }
 
+// since this is a PWA, non existing files still responde with 200,
+// so to check if a given wasm file exists we need to check the
+// returned content-type and act accordingly
 export const fetchWasm = async (url: string): Promise<any> => {
   return new Promise((resolve, reject) => {
     fetch(url).then((res) => {
