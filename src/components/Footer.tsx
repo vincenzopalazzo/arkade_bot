26311e2f22637c4fa665483f4387a586e5dd174b
João Bordalo <bordalix@users.noreply.github.com>
2025-01-02 12:54:24 +0000

    remove not used components

--- src/components/Footer.tsx
+++
@@ -1,3 +0,0 @@
-export default function Footer() {
-  return <p className='mt-2 text-xs'>Made with 🧡 by @bordalix</p>
-}
--- src/components/Pill.tsx
+++
@@ -1,9 +0,0 @@
-export default function Pill({ text }: { text: string }) {
-  return (
-    <div className='flex items-center'>
-      <p className='bg-gray-100 dark:bg-gray-800 border border-gray-100 dark:border-gray-800 px-1 rounded-md uppercase text-xxs font-semibold'>
-        {text}
-      </p>
-    </div>
-  )
-}
