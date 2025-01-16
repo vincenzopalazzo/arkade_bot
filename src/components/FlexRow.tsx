0e3cfffb82b8d2f3178f8c3c15c4ccf36cfd793b
João Bordalo <bordalix@users.noreply.github.com>
2025-01-16 16:42:16 +0000

    add cursor point when onClick

--- src/components/FlexRow.tsx
+++ src/components/FlexRow.tsx
@@ -12,6 +12,7 @@ export default function FlexRow({ alignItems, between, children, gap, onClick }:
   const justifyContent = between ? 'space-between' : 'start'
   const style = {
     alignItems: alignItems ?? 'center',
+    cursor: onClick ? 'pointer' : 'default',
     display: 'flex',
     gap: gap ?? '.5rem',
     justifyContent,
