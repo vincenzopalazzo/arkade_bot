16c150afe1c6ff1ce7f3926365c72f1e0ecf81a8
João Bordalo <bordalix@users.noreply.github.com>
2025-01-27 14:20:09 +0000

    set autofocus on inputAmount

--- src/components/InputAmount.tsx
+++ src/components/InputAmount.tsx
@@ -1,10 +1,11 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import { IonInput, IonText } from '@ionic/react'
 import { FiatContext } from '../providers/fiat'
 import { prettyNumber } from '../lib/format'
 import InputContainer from './InputContainer'
 
 interface InputAmountProps {
+  focus?: boolean
   label?: string
   onChange: (arg0: any) => void
   onFocus?: () => void
@@ -12,11 +13,17 @@ interface InputAmountProps {
   value?: number
 }
 
-export default function InputAmount({ label, onChange, onFocus, right, value }: InputAmountProps) {
+export default function InputAmount({ focus, label, onChange, onFocus, right, value }: InputAmountProps) {
   const { toUSD } = useContext(FiatContext)
 
   const [error, setError] = useState('')
   const [fiatValue, setFiatValue] = useState('')
+
+  const input = useRef<HTMLIonInputElement>(null)
+
+  useEffect(() => {
+    if (focus) input.current?.setFocus()
+  }, [])
 
   useEffect(() => {
     setFiatValue(prettyNumber(toUSD(value ?? 0), 2))
@@ -32,7 +39,13 @@ export default function InputAmount({ label, onChange, onFocus, right, value }: 
   return (
     <>
       <InputContainer error={error} label={label} right={right}>
-        <IonInput onIonFocus={onFocus} onIonInput={handleInput} type='number' value={value ? value : undefined}>
+        <IonInput
+          onIonFocus={onFocus}
+          onIonInput={handleInput}
+          ref={input}
+          type='number'
+          value={value ? value : undefined}
+        >
           <IonText slot='end' style={{ color: 'var(--dark50)', fontSize: '13px' }}>{`$${fiatValue}`}</IonText>
         </IonInput>
       </InputContainer>
