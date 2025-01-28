aa3bab5abbf504e97f2ed735bac92f4f93781796
João Bordalo <bordalix@users.noreply.github.com>
2025-01-28 18:30:24 +0000

    expand addresses

---
+++ src/components/ExpandAddresses.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import Text, { TextSecondary } from './Text'
+import { prettyLongText } from '../lib/format'
+import ChevronDownIcon from '../icons/ChevronDown'
+import ChevronUpIcon from '../icons/ChevronUp'
+import CopyIcon from '../icons/Copy'
+import FlexCol from './FlexCol'
+import FlexRow from './FlexRow'
+import Shadow from './Shadow'
+import { copyToClipboard } from '../lib/clipboard'
+import CheckMarkIcon from '../icons/CheckMark'
+
+interface ExpandAddressesProps {
+  bip21uri: string
+  boardingAddr: string
+  offchainAddr: string
+}
+
+export default function ExpandAddresses({ bip21uri, boardingAddr, offchainAddr }: ExpandAddressesProps) {
+  const [copied, setCopied] = useState('')
+  const [expand, setExpand] = useState(false)
+
+  const canCopy = navigator.clipboard && 'writeText' in navigator.clipboard
+
+  const handleCopy = async (value: string) => {
+    await copyToClipboard(value)
+    setCopied(value)
+  }
+
+  const ExpandLine = ({ title, value }: { title: string; value: string }) => (
+    <FlexRow between>
+      <FlexCol gap='0'>
+        <TextSecondary>{title}</TextSecondary>
+        <Text>{prettyLongText(value)}</Text>
+      </FlexCol>
+      <Shadow flex onClick={() => handleCopy(value)}>
+        {copied === value ? <CheckMarkIcon /> : <CopyIcon />}
+      </Shadow>
+    </FlexRow>
+  )
+
+  return (
+    <>
+      <Shadow>
+        <FlexRow between onClick={() => setExpand(!expand)}>
+          <Text>Expand address</Text>
+          {expand ? <ChevronDownIcon /> : <ChevronUpIcon />}
+        </FlexRow>
+      </Shadow>
+      {expand ? (
+        <div style={{ padding: '0 0.5rem', width: '100%' }}>
+          <FlexCol gap='0.21rem'>
+            <ExpandLine title='BIP21' value={bip21uri} />
+            <ExpandLine title='BTC address' value={boardingAddr} />
+            <ExpandLine title='Ark address' value={offchainAddr} />
+          </FlexCol>
+        </div>
+      ) : null}
+    </>
+  )
+}
