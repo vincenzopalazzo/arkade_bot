019c92c4158c1523371e421a80a5486129c05879
João Bordalo <bordalix@users.noreply.github.com>
2025-02-05 16:45:15 +0000

    fix Unlock showing wrong password

--- src/screens/Wallet/Unlock.tsx
+++ src/screens/Wallet/Unlock.tsx
@@ -48,12 +48,12 @@ export default function Unlock() {
       <Header text='Unlock' />
       <Content>
         <Padded>
-          <form>
-            <FlexCol gap='1rem'>
-              {unlocking ? null : <InputPassword focus label='Insert password' onChange={handleChange} />}
-              <Error error={Boolean(error)} text={error} />
-            </FlexCol>
-          </form>
+          <FlexCol gap='1rem'>
+            {unlocking ? null : (
+              <InputPassword focus label='Insert password' onChange={handleChange} onEnter={handleUnlock} />
+            )}
+            <Error error={Boolean(error)} text={error} />
+          </FlexCol>
         </Padded>
       </Content>
       <ButtonsOnBottom>
