04d8265ac12119f37dc0af9290ac3ae5bb39db01
João Bordalo <bordalix@users.noreply.github.com>
2025-02-14 13:13:53 +0100

    active new service worker on install

--- src/service-worker.js
+++ src/service-worker.js
@@ -70,4 +70,8 @@ self.addEventListener('message', (event) => {
   }
 })
 
+self.addEventListener('install', () => {
+  self.skipWaiting()
+})
+
 // Any other custom service worker logic can go here.
