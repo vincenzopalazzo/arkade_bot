25036ceb95623201ab80e3c910529ecc6f6fac76
João Bordalo <bordalix@users.noreply.github.com>
2025-03-01 12:30:41 +0000

    adds ion refresher

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,11 @@
-import { IonContent } from '@ionic/react'
+import { IonContent, IonRefresher, IonRefresherContent } from '@ionic/react'
 import { ReactNode } from 'react'
 
+const handleRefresh = () => {
+  setTimeout(() => {
+    window.location.reload()
+  }, 1000)
+}
 interface ContentProps {
   children: ReactNode
 }
@@ -8,6 +13,9 @@ interface ContentProps {
 export default function Content({ children }: ContentProps) {
   return (
     <IonContent>
+      <IonRefresher slot='fixed' onIonRefresh={handleRefresh}>
+        <IonRefresherContent />
+      </IonRefresher>
       <div style={{ height: '100%', paddingTop: '2rem' }}>{children}</div>
     </IonContent>
   )
