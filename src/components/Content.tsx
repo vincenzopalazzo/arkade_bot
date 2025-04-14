8a124d7055f0d99ef77083b1b72993fdbf512df1
João Bordalo <bordalix@users.noreply.github.com>
2025-04-14 17:47:45 +0100

    push to reload does not lock wallet

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,19 +1,24 @@
 import { IonContent, IonRefresher, IonRefresherContent } from '@ionic/react'
-import { ReactNode } from 'react'
-
-const handleRefresh = () => {
-  setTimeout(() => {
-    window.location.reload()
-  }, 1000)
-}
+import { ReactNode, useContext, useRef } from 'react'
+import { WalletContext } from '../providers/wallet'
 interface ContentProps {
   children: ReactNode
 }
 
 export default function Content({ children }: ContentProps) {
+  const { reloadWallet } = useContext(WalletContext)
+
+  const refresher = useRef<HTMLIonRefresherElement>(null)
+
+  const handleRefresh = () => {
+    setTimeout(() => {
+      reloadWallet().then(() => refresher.current?.complete())
+    }, 1000)
+  }
+
   return (
     <IonContent>
-      <IonRefresher slot='fixed' onIonRefresh={handleRefresh}>
+      <IonRefresher onIonRefresh={handleRefresh} ref={refresher} slot='fixed'>
         <IonRefresherContent />
       </IonRefresher>
       <div style={{ height: '100%', paddingTop: '2rem' }}>{children}</div>
