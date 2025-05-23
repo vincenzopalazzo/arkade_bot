c5108f3403291158d5a0013cd97a924b2779956f
João Bordalo <bordalix@users.noreply.github.com>
2025-05-23 11:23:13 +0100

    Sentry expects DSN in a ENV VAR (#82)

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ import { IframeProvider } from './providers/iframe'
 import * as Sentry from '@sentry/react'
 
 Sentry.init({
-  dsn: 'https://155067b98b323b6e06845b78a4f9adb0@o4508966055313408.ingest.de.sentry.io/4508970382131280',
+  dsn: import.meta.env.VITE_SENTRY_DSN,
 })
 
 const isValidUrl = (url: string) => {
