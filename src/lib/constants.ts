7ec9bba4606e3d772c70eb297de7e4d8f4014074
tiero <3596602+tiero@users.noreply.github.com>
2025-06-29 02:23:58 +0200

    chore: arkade url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ export const arknoteHRP = 'arknote'
 export const defaultFee = 0
 export const testDomains = ['dev.arkade.money', 'next.arkade.money', 'localhost']
 export const testServer = 'https://staging.mutinynet.arkade.sh'
-export const mainServer = 'https://arkd.arkade.money'
+export const mainServer = 'https://arkd.arkade.sh'
 
 export const defaultArkServer = () => {
   if (import.meta.env.VITE_ARK_SERVER) return import.meta.env.VITE_ARK_SERVER
